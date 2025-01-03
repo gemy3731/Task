14a18491b8f18d9948540996711915510b6d4399
gemy3731 <mohamedgamalz3731@gmail.com>
2025-01-03 19:10:44 +0200

    fixing2

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: 'dist',
-
-  webpack: (config:any) => {
-
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, 'src'),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
